a6d3a7617f18892c8e2601a539b8777878347cef
Jamison Cozart <jamisoncozart@gmail.com>
2020-03-16 15:43:09 -0700

    Update controllers to handle post request from Create route and show all Restaurants from List collection from DB

--- RestaurantRater/Controllers/HomeController.cs
+++ RestaurantRater/Controllers/HomeController.cs
@@ -1,6 +1,6 @@
 using Microsoft.AspNetCore.Mvc;
 
-namespace Restaurant.Controllers
+namespace RestaurantRater.Controllers
 {
   public class HomeController : Controller
   {
--- RestaurantRater/Controllers/RestaurantsController.cs
+++ RestaurantRater/Controllers/RestaurantsController.cs
@@ -1,13 +1,30 @@
 using Microsoft.AspNetCore.Mvc;
+using RestaurantRater.Models;
+using System.Collections.Generic;
 
-namespace Restaurant.Controllers
+namespace RestaurantRater.Controllers
 {
   public class RestaurantsController : Controller
   {
     [HttpGet("/restaurants")]
     public ActionResult Index()
     {
+      List<Restaurant> allRestaurants = Restaurant.GetAll();
+      return View(allRestaurants);
+    }
+
+    [HttpGet("/restaurants/new")]
+    public ActionResult New()
+    {
       return View();
+    }
+
+    [HttpPost("/restaurants")]
+    public ActionResult Create(string restaurantName)
+    {
+      Restaurant newRestaurant = new Restaurant(restaurantName);
+      newRestaurant.Save();
+      return RedirectToAction("Index");
     }
   }
 }
